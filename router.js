a6d365500c010a6b59129f4bfbd632e474ef1c61
Pedro Marques Fr <pedromarques1011@gmail.com>
2021-04-10 19:18:01 -0300

    feito ex bonus 1 e 2

--- router.js
+++ router.js
@@ -22,6 +22,19 @@ router.get("/listPatientWithSurgerie", (req, res) => {
   }).then((resp) => res.json(resp));
 });
 
+router.get("/listPatientWithSurgeriePrivate", async (req, res) => {
+  const resp = await Patient.findAll({
+    include: [{ model: Surgerie, as: "surgery" }],
+  });
+  resp.forEach(({ surgery }, idx1) =>
+    surgery.forEach(
+      ({ doctor }, idx2) => (resp[idx1].surgery[idx2].doctor = undefined)
+    )
+  );
+
+  return res.json(resp);
+});
+
 router.get("/listSurgerieWithPatient", (req, res) => {
   console.log("entrei");
   Surgerie.findAll({
@@ -35,6 +48,19 @@ router.get("/patientsByPlan/:id", async (req, res) => {
     const patientsWithPlan = await Patient.findAll({ where: { plan_id: id } });
     return res.json(patientsWithPlan);
   } catch (err) {
+    res.send("deu ruim");
+    return err;
+  }
+});
+
+router.post("/patient", async (req, res) => {
+  const { fullname, plan_id } = req.body;
+
+  try {
+    const resp = await Patient.create({ fullname, plan_id });
+    return res.json(resp);
+  } catch (err) {
+    res.send("deu ruim");
     return err;
   }
 });
